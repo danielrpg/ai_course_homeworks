5e52b946d77bd1a48cffa55db4c32946727ee770
danielrpg <daniel.fernandezgp@gmail.com>
2025-11-10 15:43:55 -0400

    el problema propuesto

--- tabla_multiplicar_1.py
+++ tabla_multiplicar_1.py
@@ -1,3 +1,10 @@
+'''
+    Ejercicio Propuesto:
+    Mostrar la tabla de multiplicar de cualquier numero ingresado por teclado FOR
+    Salida: 1x5=5
+            2x5=10
+'''
+
 
 numero = int(input("Ingrese un número para mostrar su tabla de multiplicar: "))
 
